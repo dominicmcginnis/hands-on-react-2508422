996f19af3f0bba9446a075bc1d2c0ab3853bb157
Dominic McGinnis <Dominic.McGinnis@elliemae.com>
2023-07-25 08:49:39 -0700

    my solution to next/prev

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,21 +1,51 @@
-export default ({ member, handleClose }) => {
+export default ({
+  member,
+  prevMember,
+  nextMember,
+  handleClose,
+  handleNext,
+  handlePrev,
+}) => {
   return (
     <dialog id="modal-member" open>
       <article>
-        <a href="#close"
-          aria-label="Close"
-          className="close"
-          data-target="modal-member"
-          onClick={handleClose}
-        ></a>
+        <nav>
+          <ul>
+            <li>
+              <button onClick={handlePrev} disabled={!prevMember}>
+                Previous
+              </button>
+            </li>
+            <li>
+              <button onClick={handleNext} disabled={!nextMember}>
+                Next
+              </button>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a
+                href="#close"
+                aria-label="Close"
+                className="close"
+                data-target="modal-member"
+                onClick={handleClose}
+              ></a>
+            </li>
+          </ul>
+        </nav>
         <hgroup>
-          <div style={{
-            display: "flex",
-            gap: '1rem'
-          }}>
-            <img style={{ width: '200px' }}
+          <div
+            style={{
+              display: "flex",
+              gap: "1rem",
+            }}
+          >
+            <img
+              style={{ width: "200px" }}
               src={`images/${member.slug}.svg`}
-              alt={member.name} />
+              alt={member.name}
+            />
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
@@ -24,5 +54,5 @@ export default ({ member, handleClose }) => {
         </hgroup>
       </article>
     </dialog>
-  )
-}
+  );
+};
